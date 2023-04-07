3e8372d34371c0dc4f608cd240e9be65be2d5f65
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-04-07 11:54:04 +0600

    react router added

--- src/components/description/LineUnlike.js
+++ src/components/description/LineUnlike.js
@@ -4,20 +4,20 @@ import unLikeImg from '../../assets/unlike.svg'
 
 const LineUnlike = () => {
     return (
-        <div class="flex gap-10 w-48">
-            <div class="flex gap-1">
-                <div class="shrink-0">
-                    <img class="w-5 block" src={likeImg} alt="Like" />
+        <div className="flex gap-10 w-48">
+            <div className="flex gap-1">
+                <div className="shrink-0">
+                    <img className="w-5 block" src={likeImg} alt="Like" />
                 </div>
-                <div class="text-sm leading-[1.7142857] text-slate-600">
+                <div className="text-sm leading-[1.7142857] text-slate-600">
                     100K
                 </div>
             </div>
-            <div class="flex gap-1">
-                <div class="shrink-0">
-                    <img class="w-5 block" src={unLikeImg} alt="Unlike" />
+            <div className="flex gap-1">
+                <div className="shrink-0">
+                    <img className="w-5 block" src={unLikeImg} alt="Unlike" />
                 </div>
-                <div class="text-sm leading-[1.7142857] text-slate-600">
+                <div className="text-sm leading-[1.7142857] text-slate-600">
                     100K
                 </div>
             </div>
